302b6bb0ef9c33eb9b5a3b16ed31a925872364e7
lewisgabin <lewisgabin27@gmail.com>
2023-02-04 13:08:33 -0400

    feat: :lipstick: adding the view to create a task

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import Form from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-    <h1>Todo List</h1>
-    </div>
-  )
+    <>
+      <header>
+        <h1 id="title">Todo List</h1>
+      </header>
+      <div className="container">
+        <Form />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+import { PlusIcon } from "@heroicons/react/24/outline";
+const Form = () => {
+  const handleFormSubmit = (e) => {
+    e.preventDefaul();
+    console.log(e.target.value);
+  };
+  return (
+    <form className="taskForm" onSubmit={handleFormSubmit}>
+      <div className="wrapper">
+        <input
+          type="text"
+          className="input"
+          id="task"
+          required
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task" className="label">
+          Enter Task
+        </label>
+      </div>
+      <button type="submit" className="btn" aria-label="Add">
+     
+        <PlusIcon className="icon"/>
+      </button>
+    </form>
+  );
+};
+
+export default Form;
